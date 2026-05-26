a15930277e9a961f35bdc1fb65ce5af30498a8d7
olivvya8 <oliviac.woollett@gmail.com>
2026-05-27 09:04:40 +1200

    includes edited h file

---
+++ logTheft.h
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "camera_x11.h"
+#include <time.h>
+#include <stdbool.h>
+#include <math.h>
+
+char *days[] = {
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+};
+
+char *months[] = {
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+};
+
+void logTheft(struct tm *current){
+
+	time_t raw_time;
+	struct tm *info;
+	
+	FILE *fptr = fopen("ruby_theft.log", "a"); //error - need to create a new file, currently outputs time several times
+
+	if(fptr == NULL){
+		perror("Failed to open log file");
+		return;
+	}
+
+	int hours = current->tm_hour;
+	int minutes = current->tm_min;
+	int seconds = current->tm_sec;
+	int wday = current->tm_wday;
+	int mday = current->tm_mday;
+	int month = current->tm_mon + 1;
+	int year = current->tm_year + 1900;
+
+	char *dayName = days[current->tm_wday];
+	char *monthName = months[current->tm_mon];
+
+
+	fprintf(fptr, "Date and time of attempted theft: \n");
+	fprintf(fptr, "%s %d %s %d\n", dayName, mday, monthName, year);
+	fprintf(fptr, "%02d hours - %02d minutes - %02d seconds\n\n", hours, minutes, seconds);
+	
+	fclose(fptr);
+
+}
---
+++ unit_logTheft.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "camera_x11.h"
+#include <time.h>
+#include <stdbool.h>
+#include <math.h>
+#include "logTheft.h"
+
+
+int main() {
+
+    if (start_camera() != 0) {
+        printf("Failed to start camera\n");
+        return 1;
+    }
+
+    time_t raw_time;
+	struct tm *info;
+    
+    time(&raw_time);
+    info = localtime(&raw_time);
+    int hours = info->tm_hour;
+    int minutes = info->tm_min;
+    int seconds = info->tm_sec;
+    int wday = info->tm_wday;
+    int mday = info->tm_mday;
+    int month = info->tm_mon + 1;
+    int year = info->tm_year + 1900;
+
+    for (int i=0;i<50;i++) {
+
+        take_picture();
+
+        logTheft(info);
+
+        display_picture();
+    }
+
+    printf("Test complete\n");
+    return 0;
+}
+
